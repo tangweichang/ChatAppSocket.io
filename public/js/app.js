881411c7e8d22544e9251ac7cbaf45219a005400
tangweichang <tangweichangjlu@gmail.com>
2015-11-09 02:13:31 -0500

    Add timestamp on UI

--- public/js/app.js
+++ public/js/app.js
@@ -4,10 +4,11 @@ socket.on('connect', function() {
 });
 
 socket.on('message', function(message) {
+    var momentTimestamp = moment.utc(message.timestamp);
     console.log('New message:');
     console.log(message.text);
     
-    jQuery('.messages').append('<p>' + message.text + '</p>');
+    jQuery('.messages').append('<p>' + momentTimestamp.local().format('h:mm a') + ': ' + message.text + '</p>');
     
 });
 
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var moment = require('moment');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -10,13 +11,15 @@ io.on('connection', function(socket) {
     console.log('User connected via socket.io!'); 
     socket.on('message', function(message) {
         console.log('Message received: ' + message.text);
+        message.timestamp = moment.valueOf();
         
         io.emit('message', message);
         //socket.broadcast.emit('message', message);
         
     });
     socket.emit('message', {
-        text: 'Welcome to the chat application!'   
+        text: 'Welcome to the chat application!',
+        timestamp: moment.valueOf()
     });
 });
 
