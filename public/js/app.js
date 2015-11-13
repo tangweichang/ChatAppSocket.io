faee19d69e5aeb5e0289f88be88fe41efbbfc5cc
tangweichang <tangweichangjlu@gmail.com>
2015-11-12 21:32:52 -0500

    add name

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,18 @@
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
 var socket = io();
+
 socket.on('connect', function() {
     console.log('Connected to socket.io server!');  
 });
 
 socket.on('message', function(message) {
     var momentTimestamp = moment.utc(message.timestamp);
+    var $message = jQuery('.messages');
     console.log('New message:');
     console.log(message.text);
-    
-    jQuery('.messages').append('<p>' + momentTimestamp.local().format('h:mm a') + ': ' + message.text + '</p>');
+    $message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
+    $message.append('<p>' + message.text + '</p>');
     
 });
 
@@ -18,7 +22,9 @@ var $form = jQuery('#message-form');
 $form.on('submit',function(event) {
     event.preventDefault();
     var $message = $form.find('input[name=message]');
+    
     socket.emit('message', {
+        name: name,
         text: $message.val()
     });
     
