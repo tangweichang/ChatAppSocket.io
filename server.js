a5f4a579d7a05c2e14f2099f0205042f56d42e53
tangweichang <tangweichangjlu@gmail.com>
2015-11-13 02:03:08 -0500

    add @currentUsers command

--- server.js
+++ server.js
@@ -9,6 +9,31 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+function sendCurrentUsers (socket) {
+    var info = clientInfo[socket.id];
+    var users = [];
+    if (typeof info === 'undefined') {
+        return;   
+        
+    }
+    
+    Object.keys(clientInfo).forEach(function(socketId) {
+        var userInfo = clientInfo[socketId];
+        if (info.room === userInfo.room) {
+            users.push(userInfo.name);   
+        }
+        
+        
+    });
+    
+    socket.emit('message', {
+        name: 'System',
+        text: 'Current users: ' + users.join(', '),
+        timestamp: moment().valueOf()
+        
+    });
+}
+
 io.on('connection', function(socket) {
     console.log('User connected via socket.io!'); 
     
@@ -42,9 +67,16 @@ io.on('connection', function(socket) {
     
     socket.on('message', function(message) {
         console.log('Message received: ' + message.text);
-        message.timestamp = moment.valueOf();
         
-        io.to(clientInfo[socket.id].room).emit('message', message);
+        if (message.text === '@currentUsers') {
+            sendCurrentUsers(socket);   
+        } else {
+            message.timestamp = moment.valueOf();
+        
+            io.to(clientInfo[socket.id].room).emit('message', message);
+        }
+        
+        
         //socket.broadcast.emit('message', message);
         
     });
